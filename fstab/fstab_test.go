5045114593655b736bb6e6a4dd2e070bb5a78692
Sascha Loishandl <sascha.loishandl@gmail.com>
2026-05-26 11:31:11 +0200

    test

--- fstab/fstab_test.go
+++ fstab/fstab_test.go
@@ -36,13 +36,47 @@ UUID=36614       /home   ext4    defaults,noatime       0       2
 	assert.Equal(t, "UUID=7a235ef76a", rootFs.Device)
 	assert.Equal(t, "/", rootFs.Mountpoint)
 	assert.Equal(t, "ext4", rootFs.FsType)
-	assert.Equal(t, 3, len(rootFs.Options))
-	assert.Equal(t, "rw", rootFs.Options[0])
-	assert.Equal(t, "relatime", rootFs.Options[1])
-	assert.Equal(t, "stripe=4", rootFs.Options[2])
+	assert.Equal(t, []string{"rw", "relatime", "stripe=4"}, rootFs.Options)
 	assert.Equal(t, "0", rootFs.Dump)
 	assert.Equal(t, "1", rootFs.Fsck)
 
-	assert.Equal(t, "UUID=dbe746", entries[4].Device)
-	assert.Equal(t, "/mnt/disk2", entries[4].Mountpoint)
+	bootFs := entries[1]
+	assert.Equal(t, "UUID=9e6757da", bootFs.Device)
+	assert.Equal(t, "/boot", bootFs.Mountpoint)
+	assert.Equal(t, "ext4", bootFs.FsType)
+	assert.Equal(t, []string{"rw", "relatime", "stripe=32"}, bootFs.Options)
+	assert.Equal(t, "0", bootFs.Dump)
+	assert.Equal(t, "2", bootFs.Fsck)
+
+	efiFs := entries[2]
+	assert.Equal(t, "UUID=E90A-1DBE", efiFs.Device)
+	assert.Equal(t, "/boot/efi", efiFs.Mountpoint)
+	assert.Equal(t, "vfat", efiFs.FsType)
+	assert.Equal(t, []string{"rw", "relatime", "fmask=0022", "dmask=0022", "codepage=437", "iocharset=ascii", "shortname=mixed", "utf8", "errors=remount-ro"}, efiFs.Options)
+	assert.Equal(t, "0", efiFs.Dump)
+	assert.Equal(t, "2", efiFs.Fsck)
+
+	homeFs := entries[3]
+	assert.Equal(t, "UUID=36614", homeFs.Device)
+	assert.Equal(t, "/home", homeFs.Mountpoint)
+	assert.Equal(t, "ext4", homeFs.FsType)
+	assert.Equal(t, []string{"defaults", "noatime"}, homeFs.Options)
+	assert.Equal(t, "0", homeFs.Dump)
+	assert.Equal(t, "2", homeFs.Fsck)
+
+	disk2Fs := entries[4]
+	assert.Equal(t, "UUID=dbe746", disk2Fs.Device)
+	assert.Equal(t, "/mnt/disk2", disk2Fs.Mountpoint)
+	assert.Equal(t, "ext4", disk2Fs.FsType)
+	assert.Equal(t, []string{"defaults", "noatime"}, disk2Fs.Options)
+	assert.Equal(t, "0", disk2Fs.Dump)
+	assert.Equal(t, "2", disk2Fs.Fsck)
+
+	mediaFs := entries[5]
+	assert.Equal(t, "//192.168.0.100/Media", mediaFs.Device)
+	assert.Equal(t, "/mnt/media", mediaFs.Mountpoint)
+	assert.Equal(t, "cifs", mediaFs.FsType)
+	assert.Equal(t, []string{"credentials=/etc/samba/creds/my_server_creds", "uid=1000", "gid=1000", "file_mode=0664", "dir_mode=0775", "iocharset=utf8", "vers=3.0", "nofail", "soft", "noserverino", "x-systemd.automount", "_netdev"}, mediaFs.Options)
+	assert.Equal(t, "0", mediaFs.Dump)
+	assert.Equal(t, "0", mediaFs.Fsck)
 }
